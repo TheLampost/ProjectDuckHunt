746bbfc1723a99ec0662154e77fd7c6427ef7c15
TheLampost <34274706+TheLampost@users.noreply.github.com>
2026-07-01 19:08:49 -0400

    Look around with mouse added
    -added looking around with mouse.  Y axis clamped to prevent player from doing 360 from top to bottom.

---
+++ ProjectDuckHunt/Assets/Scripts/LookAround.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LookAround : MonoBehaviour
+{
+    float sensitivity = 0.1f;
+    float xRotation = 0f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * sensitivity;
+        float mouseY = mouseDelta.y * sensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
+
